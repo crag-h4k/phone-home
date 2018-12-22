0431a29dd4bba9ca945322d435049a6fdf043729
l0k1 <l0k1@l0k1>
2018-12-22 13:25:24 -0700

    need to del flags

--- phone_home/phone_home.py
+++ phone_home/phone_home.py
@@ -1,7 +1,8 @@
 from subprocess import check_output
 from os import system
 from pprint import pprint
-from sys import argv
+from sys import argv, stdout
+from urllib.request import urlopen
 #from paramiko import *
 
 
@@ -13,7 +14,7 @@ class Flag:
         self.line_text = line_text
 
 def find_files(fname, loc):
-    cmd = " ".join(['find', loc, ' -name ', '\"'+ fname + '\"', ])
+    cmd = " ".join(['find', loc, ' -name ', '\"*'+ fname + '*\"', ])
     fnames = check_output(cmd, shell=True).decode('utf-8')
     fname_list = fnames.split('\n')
     i = 0
@@ -23,30 +24,41 @@ def find_files(fname, loc):
         i += 1
     return fname_list
 
-def get_key_text(url):
-    cmd = " ".join(['curl ', url])
-    check_output(cmd)
+def get_key(url):
+    key = urlopen(url).readlines()
+    if len(key) != 1: return False
+    #else: return key[0]
+    else: 
+        print(key[0])
+        return key[0]
+
+def insert_text(text, fname, line_no = None):
+    print(type(text))
+    try: text.encode()
+    except Exception as e: print(e)
+    print(type(text))
+    if line_no == None:
+        with open(fname, 'ba+') as f: f.write(text)
+    else: 
+        with open(fname, 'br') as f:
+            lines = f.readlines()
+            print(lines)
+            lines[line_no - 1] += text
+        with open(fname, 'bw+') as f:
+            for line in lines:
+                f.write(line)
     return
 
-
-def save_key_file(key_text, ssh_dir):
-    cmd = " ".join(['curl ', url])
-    run(cmd)
-    return
-
-def send_info(text):
-    return
 
 def find_flag(flag, loc):
     cmd = " ".join(["grep -iRn", flag, search_dir])
     flags = check_output(cmd, shell=True).decode('utf-8')
-    #pprint(flags)
     return [flag] + flags.split('\n')
 
-    #Flag(str_flag, fname, line_no, line_text):
 def make_flag(raw_flags):
     F_list = []
     str_flag = raw_flags[0]
+
     for flag in raw_flags:
         data = [str_flag] + flag.split(':')
         if len(data) != 4: continue
@@ -61,8 +73,12 @@ def make_flag(raw_flags):
 
         
     print(_flags)
+
 def clear_tracks(flag, logs):
     #auth, 
+    return
+
+def send_info(text):
     return
 
 if __name__ == '__main__':
@@ -71,5 +87,8 @@ if __name__ == '__main__':
     #raw_flags = find_flag(flag, search_dir)
     #F_list = make_flag(raw_flags)
     #print(F_list)
-    fname = argv[1]
-    pprint(find_files(fname, '/'))
+    #fname = argv[1]
+    #pprint(find_files(fname, '/'))
+    url = argv[1]
+    fname = argv[2]
+    insert_text('poopypants', fname, line_no = 1)
