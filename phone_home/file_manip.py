60027cecda0e32aeca1439ea5e7e5d9a633c55b7
deadlift1226 <danemorgan91@gmail.com>
2018-12-27 12:20:34 -0700

    need to make more reliable

--- phone_home/file_manip.py
+++ phone_home/file_manip.py
@@ -44,21 +44,24 @@ def x_clear_tracks(flag, fname):
             else: f.write(line)
     return
 
-def clear_tracks(Flag):
-
-    line_no = Flag.line_no - 1
-    fname = Flag.fname
-    with open(fname, 'br') as f:
-        lines = f.readlines()
-        new_lines = lines 
-        print(lines[line_no])
-        del new_lines[line_no]
+def clear_tracks(file_data_list):
+    for data in file_data_list:
+        fname = data.get('file_name')
+        x_lines  = data.get('lines')
+        x_lines.reverse()
+        print(fname, x_lines)
+        with open(fname, 'br') as f:
+            lines = f.readlines()
+            new_lines = lines 
+            #print(lines[x_lines])
+            for i in x_lines: del new_lines[i-1]
+            pprint(new_lines)
         #pprint(new_lines)
         #del new_lines[line_no + 1] 
-    with open(fname, 'bw') as f: 
-        f.seek(0)
-        for line in new_lines:
-            f.write(line)
+    #with open(fname, 'bw') as f: 
+     #   f.seek(0)
+      #  for line in new_lines:
+       #     f.write(line)
             #for line in lines:
         #    if flag in line: continue
         #    else: f.write(line)
@@ -69,5 +72,6 @@ if __name__ == '__main__':
     F_list = make_flags(find_flag(flag, '../tests'))
     data = get_line_nos(F_list)
     [print(F.fname, F.line_no, F.line_text) for F in F_list]
-    #[clear_tracks(F)for F in F_list]
+    pprint(data)
+    clear_tracks(data)
 
